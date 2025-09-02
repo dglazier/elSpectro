8fd777a03b1d40584a14784b941ad976617842e1
Derek Glazier <Derek.Glazier@glasgow.ac.uk>
2025-09-02 14:55:19 +0100

    update CollidingParticle to include DecayChannel

--- core/CollidingParticle.cpp
+++ core/CollidingParticle.cpp
@@ -1,92 +1,143 @@
 #include "CollidingParticle.h"
-#include "FunctionsForGenvector.h"
-#include "Manager.h"
+#include "DecayModel.h"
+#include "DecayVectors.h"
+#include "TRandom3.h"
+#include "TSystem.h"
+#include "TMath.h"
+#include <iostream>
 
-namespace elSpectro{
+namespace elSpectro {
 
-  ///////////////////////////////////////////////////////////
-  //cannot default construct at least need a 4-momentum
-  CollidingParticle::CollidingParticle(int pdg,Double_t momentum):
-    Particle(pdg){
-    //Set interacting particle pdg
-    _interactingPdg=pdg;
-    auto mass= PdgMass();
-    LorentzVector lv(0,0,momentum,TMath::Sqrt(momentum*momentum+mass*mass));
-    SetP4(lv);
-    _nominal=P4();
-    //_interactingParticle=P4ptr();
-    _idxInteract = -1;
+  CollidingParticle::CollidingParticle() : Particle() {
+    // Default constructor
   }
-  /////////////////////////////////////////////////////////
-  //or a model to generate a 4-momentum
-  CollidingParticle::CollidingParticle(int pdg,Double_t momentum,int parentpdg,decaymodel_ptr  model,decayer_ptr  decayer):
-    Particle(parentpdg),_model{model},_decayer{decayer}
-  {
-    //Set interacting particle pdg
-    _interactingPdg=pdg;
-    //set the parent LorentzVector
-    auto mass= PdgMass();
-    LorentzVector lv(0,0,momentum,TMath::Sqrt(momentum*momentum+mass*mass));
+
+  CollidingParticle::CollidingParticle(Int_t pdg, Double_t momentum) : Particle(pdg) {
+    // Constructor for a simple beam particle with a defined momentum.
+    _interactingPdg = pdg;
+    auto mass = PdgMass(); // Assumes Particle base class can look up mass from PDG
+    LorentzVector lv(0, 0, momentum, TMath::Sqrt(momentum * momentum + mass * mass));
     SetP4(lv);
-   _nominal=P4();
-  
-   //Find the relevent particle pointer in the model
-   //this is the particle which is used in the production process
-   UInt_t position=0;
-   for(auto& p:_model->Products()){
-       
-      if(p->Pdg()==pdg){
-	
-	if(_idxInteract!=-1){
-	  std::cerr<<"CollidingParticle::CollidingParticle, multiple particles with pdg =  "<<pdg<<std::endl; exit(0);
-	}
-	else{	
-	  //  _interactingParticle=p->P4ptr();
-	  _idxInteract = position;
+    _nominalP4 = P4();
+    _interactIdx = -1;
+  }
+
+  CollidingParticle::CollidingParticle(Int_t pdg, Double_t momentum, Int_t parentpdg, decaymodel_ptr  model, decayer_ptr  decayer)
+    : Particle(parentpdg) {
+    
+    // Set the model and decayer on the internal channel
+    _decayChannel.AddDecay(nullptr,1.,std::move(model),std::move(decayer));
+
+    // Set interacting particle pdg
+    _interactingPdg = pdg;
+
+    // Set the parent LorentzVector
+    auto mass = PdgMass();
+    LorentzVector lv(0, 0, momentum, TMath::Sqrt(momentum * momentum + mass * mass));
+    SetP4(lv);
+    _nominalP4 = P4();
+
+    // Find the relevant particle pointer in the model
+    // This is the particle which is used in the production process
+    UInt_t position = 0;
+    for (auto& p : model->Products()) {
+      if (p->Pdg() == pdg) {
+	if (_interactIdx != -1) {
+	  std::cerr << "CollidingParticle::CollidingParticle, multiple particles with pdg = " << pdg << std::endl;
+	  exit(0);
+	} else {
+	  _interactIdx = position;
 	}
       }
-      
       position++;
     }
-    
-   //We need a "nominal" 4-momentum for our interacting particle
-   //this is for integrations etc.
-    //to do this we boost it from rest into lab frame of parent
-   //Note theta= 0 from LorentzVector  lv(0,0,momentum,TMath::Sqrt(momentum*momentum+mass*mass));, so RandPhi does not matter
-   if(_idxInteract!=-1){
-     _nominal = GetInteracting4Vector();
-     _decayer->BoostToParentWithRandPhi(P4(),_nominal);
-     
-   }
-   
+
+    // We need a "nominal" 4-momentum for our interacting particle for integrations etc.
+    // To do this we boost it from rest into the lab frame of the parent.
+    if (_interactIdx != -1) {
+      // Get the interacting particle (at rest in the parent frame)
+      _nominalP4 = GetInteracting4Vector();
+      // Boost it to the lab frame
+      decayer->BoostToParentWithRandPhi(P4(), _nominalP4);
+    }
   }
-  /////////////////////////////////////////////////////////
-  /// rotate beam angles
-  void CollidingParticle::SetAngleThetaPhi(Double_t th,Double_t phi){
-      _dirTheta=th;_dirPhi=phi;
-      auto p4=_nominal; //copy 4-vector
-      //and rotate it
-      genvector::LorentzRotateY(p4,th);
-      genvector::LorentzRotateZ(p4,phi);
-      //Set the rotated vector
-      SetP4(p4);
-      _nominal=p4;
-    }
-  /////////////////////////////////////////////////////////
-  void CollidingParticle::PostInit(ReactionInfo* info){
-        //decay vertex position
-    
-    if(_model){
-      auto& products=_model->Products();
-      //same vertex as parent
-      for(auto* prod: products){
-	prod->SetVertexID(VertexID());
-      }
-    
-    _model->PostInit(info);
-    }
-    if(_decayer)_decayer->PostInit(info);
-    
+
+
+  void CollidingParticle::SetDecayChannel(const DecayChannel& channel) {
+    // Use the copy-assignment operator to copy the channel's state
+    _decayChannel = channel;
+  }
+
+
+  double CollidingParticle::Generate() {
+    // Use the decayer to generate the event kinematics for the products defined by the model.
+    if(_decayChannel.N() == 0) return 1.0;
+    auto model = Model();
+    return Decayer()->Generate(P4(), model->Products());
   }
   
-}
+  void CollidingParticle::PostInit(ReactionInfo* info) {
+    // This function handles setup after the particle is fully constructed.
+
+    // First, delegate the PostInit call to the DecayChannel.
+   // This allows the channel to initialize its internal components (model and decayer)
+   // and potentially make choices about the decay.
+    if(_decayChannel.N() == 0) return;
+    _decayChannel.PostInit(info);
+   
+   
+   auto model = Model();
+   if (model) {
+      // Get the list of product particles from the decay model.
+      auto& products = model->Products();
+
+      // Assign the same vertex ID from this particle (the parent)
+      // to all of its daughter particles.
+      for (auto* prod : products) {
+	prod->SetVertexID(this->VertexID());
+      }
+
+      // Call the PostInit function on the sub-model to continue initialization.
+      model->PostInit(info);
+    }
+  }
+    
+  Double_t CollidingParticle::GenerateComponents() {
+    auto model = Model();
+    if (model == nullptr) {
+      return 1.0;
+    }
+
+    Double_t weight = 0.0;
+    // Loop to ensure a physical event is generated (weight is not zero)
+    while (weight == 0) {
+      weight = this->Generate();
+      if (weight > 0) { // Only multiply if the generation was successful
+	weight *= model->Intensity();
+      }
+    }
+
+    // --- Apply beam divergence smearing ---
+    /*
+      if (_beamProps._divHor > 0 || _beamProps._divVer > 0) {
+      LorentzVector p4 = P4();
+
+      // Use TRandom3 for Gaussian smearing. gRandom is a ROOT global pointer.
+      if (!gRandom) gRandom = new TRandom3(0);
+
+      // Generate random smearing angles based on the beam divergence
+      double dTheta = gRandom->Gaus(0, _beamProps._divVer); // Vertical divergence affects theta
+      double dPhi = gRandom->Gaus(0, _beamProps._divHor);   // Horizontal divergence affects phi
+        
+      // Apply the smearing by rotating the momentum vector
+      p4.RotateY(dTheta);
+      p4.RotateZ(dPhi);
+
+      // Set the new, smeared four-momentum for the particle
+      SetP4(p4);
+      }
+    */
+    return weight;
+  }
+
+} // namespace elSpectro
--- core/CollidingParticle.h
+++ core/CollidingParticle.h
@@ -1,103 +1,225 @@
-//////////////////////////////////////////////////////////////
-///
-///Class:		CollidingParticle
-///Description:
-///             1) Class for generating initial state LorentzVector
-/// 
+/**
+ * @file CollidingParticle.h
+ * @author D. Glazier
+ * @brief Defines the CollidingParticle class.
+ * @date 2025-09-02
+ */
 
-
-#pragma once
+#ifndef COLLIDINGPARTICLE_H
+#define COLLIDINGPARTICLE_H
 
 #include "Particle.h"
-#include "DecayChannel.h"
-//#include "DecayVectors.h"
+#include "DecayChannel.h" // Include the DecayChannel header
+#include "DecayModel.h"   // For accessing product particles
+#include "FunctionsForGenvector.h"
 
-namespace elSpectro{
-  
- 
-  class CollidingParticle : public Particle {
+namespace elSpectro {
 
-  public:
+class DecayVectors; // Forward declaration
+
+/**
+ * @struct BeamProperties
+ * @brief A structure to hold properties of a beam particle.
+ */
+struct BeamProperties {
+    Double_t _dirTheta = 0.0;       ///< Beam polar angle in radians.
+    Double_t _dirPhi = 0.0;         ///< Beam azimuthal angle in radians.
+    Double_t _sizeHor = 0.0;        ///< Horizontal beam spot size in cm.
+    Double_t _sizeVer = 0.0;        ///< Vertical beam spot size in cm.
+    Double_t _divHor = 0.0;         ///< Horizontal beam divergence in radians.
+    Double_t _divVer = 0.0;         ///< Vertical beam divergence in radians.
+};
+
+
+/**
+ * @class CollidingParticle
+ * @brief Represents a particle that participates in an initial collision, like a beam or target.
+ */
+class CollidingParticle : public Particle {
+public:
+    /**
+     * @brief Default constructor.
+     */
+    CollidingParticle();
+
+    /**
+     * @brief Constructor for a simple beam particle with a defined momentum.
+     * @param[in] pdg The PDG code of the interacting particle.
+     * @param[in] momentum The momentum magnitude along the z-axis in GeV/c.
+     */
+    CollidingParticle(Int_t pdg, Double_t momentum);
     
-    CollidingParticle()=delete;
-    //cannot default construct at least need a 4-momentum
-    CollidingParticle(int pdg,Double_t momentum);
-    //or a model to generate a 4-momentum
-    CollidingParticle(int pdg,Double_t momentum,int parentpdg,decaymodel_ptr  model,decayer_ptr  decayer);
- 
-    DecayModel*  Model()const {return _model.get();}
+    /**
+     * @brief Constructor for a particle that is a product of another decay.
+     * @param[in] pdg The PDG code of the interacting particle from the parent's decay.
+     * @param[in] momentum The momentum of the parent particle in GeV/c.
+     * @param[in] parentpdg The PDG code of the parent particle.
+     * @param[in] model The DecayModel describing the parent's decay.
+     * @param[in] decayer The DecayVectors describing the parent's decay kinematics.
+     */
+    CollidingParticle(Int_t pdg, Double_t momentum, Int_t parentpdg, decaymodel_ptr  mod,decayer_ptr  dec);
 
-    double Generate(){
-       if(_decayer.get()==nullptr) return 1.0;
-      return _decayer->Generate(P4(),_model->Products());
+
+    /**
+     * @brief Sets the decay channel for this particle by copying from another.
+     * @param[in] channel A constant reference to the DecayChannel object to copy.
+     */
+    void SetDecayChannel(const DecayChannel& channel);
+
+    /**
+     * @brief Gets a mutable reference to the internal DecayChannel object.
+     * @return A reference to the DecayChannel object.
+     */
+    DecayChannel& GetDecayChannel() { return _decayChannel; }
+
+    /**
+     * @brief Gets the DecayModel from the associated channel.
+     * @return A pointer to the DecayModel, or nullptr if not set.
+     */
+  DecayModel* Model()  const {
+    // Get the primary DecayModel from the owned DecayChannel.
+    return (_decayChannel.N() == 0) ? nullptr:_decayChannel.CurrModel();
+  }
+
+  /**
+   * @brief Gets the DecayVectors from the associated channel.
+   * @return A pointer to the DecayVectors, or nullptr if not set.
+   */
+  DecayVectors* Decayer() {
+    // Get the primary DecayVectors from the owned DecayChannel.
+    return  (_decayChannel.N() == 0) ? nullptr:_decayChannel.CurrDecayer();
+  }  
+  /**
+     * @brief Generates the final state kinematics for the event.
+     * @details This is the core generation step, delegating to the DecayVectors object.
+     * @return The physics weight of the generated event.
+     */
+    double Generate();
+
+    /**
+     * @brief Generates a complete event, including intensity and beam effects.
+     * @details This method calls Generate() and then applies the model's intensity
+     * and any configured beam smearing effects.
+     * @return The final, weighted result for the event.
+     */
+    Double_t GenerateComponents();
+
+  /**
+     * @brief Post-initialization routine for the particle.
+     * @details This function is called after the main object construction to perform
+     * any setup that requires external information, such as linking decay products
+     * to the correct vertex and initializing sub-models.
+     * @param[in] info Pointer to the global ReactionInfo object.
+     */
+    void PostInit(ReactionInfo* info);
+
+    // --- Setters for beam/target properties ---
+
+    /**
+     * @brief Sets the nominal four-momentum of the beam particle.
+     * @param[in] p4 The nominal four-momentum.
+     */
+    void SetNominalP4(const LorentzVector& p4) { _nominalP4 = p4; }
+
+    /**
+     * @brief Sets the PDG code of the particle this particle interacts with.
+     * @param[in] pdg The PDG code of the interaction partner.
+     */
+    void SetInteractingPdg(Int_t pdg) { _interactingPdg = pdg; }
+    
+    /**
+     * @brief Sets the index of the interacting product particle from the DecayModel.
+     * @param[in] idx The index in the product list. Use -1 for the nominal beam.
+     */
+    void SetInteractingIndex(Int_t idx) { _interactIdx = idx; }
+
+    /**
+     * @brief Sets the central direction of the beam.
+     * @param[in] theta The polar angle in radians.
+     * @param[in] phi The azimuthal angle in radians.
+     */
+    void SetAngleThetaPhi(Double_t theta, Double_t phi) {
+      _beamProps._dirTheta = theta; _beamProps._dirPhi = phi;
+      auto p4=_nominalP4; //copy 4-vector
+      //and rotate it
+      genvector::LorentzRotateY(p4,theta);
+      genvector::LorentzRotateZ(p4,phi);
+      //Set the rotated vector
+      SetP4(p4);
+      _nominalP4=p4;
     }
 
-    const DecayVectors* Decayer() const {return _decayer.get();}
-    void SetDecayer(DecayVectors* decayer){_decayer.reset(decayer);}
-    
-    Double_t  GenerateComponents(){
-      if(_model==nullptr )return 1.0;
-      
-      Double_t weight=0;
-      while((weight)==0){ //sample until physical
-	weight=Generate();
-	weight*=_model->Intensity();
-      }
-      // can add beam divergence etc. here
-      // ...
-      return weight;
-    }
-    DecayType IsDecay() const noexcept final {return DecayType::Production;}
+    /**
+     * @brief Sets the beam spot size at the interaction point.
+     * @param[in] hor The horizontal size (e.g., sigma) in cm.
+     * @param[in] ver The vertical size (e.g., sigma) in cm.
+     */
+    void SetBeamSpotSize(Double_t hor, Double_t ver) { _beamProps._sizeHor = hor; _beamProps._sizeVer = ver; }
 
-    void PostInit(ReactionInfo* info) ;
-    
-    // const LorentzVector* GetInteracting4Vector() const {return _interactingParticle;}
-    // const LorentzVector* GetNominal4Vector() const {return &_nominal;}
+    /**
+     * @brief Sets the beam divergence.
+     * @param[in] hor The horizontal divergence (e.g., sigma) in radians.
+     * @param[in] ver The vertical divergence (e.g., sigma) in radians.
+     */
+    void SetBeamDivergence(Double_t hor, Double_t ver) { _beamProps._divHor = hor; _beamProps._divVer = ver; }
 
-    //if decays return current decay P4, if not return my P4
+    // --- Getters for beam/target properties ---
+    
+    const LorentzVector& GetNominal4Vector() const { return _nominalP4; }
+    Int_t GetInteractingPdg() const { return _interactingPdg; }
+    Int_t GetInteractingIndex() const { return _interactIdx; }
+    const BeamProperties& GetBeamProperties() const { return _beamProps; }
+
+    /**
+     * @brief Gets the four-vector of the particle to be used in the interaction.
+     * @details If the interacting index is -1, this returns the nominal beam P4.
+     * Otherwise, it returns the P4 of the corresponding product particle from the decay model.
+     * @return A constant reference to the interacting four-vector.
+     */
     const LorentzVector& GetInteracting4Vector() const {
-      return _idxInteract==-1 ? GetNominal4Vector() : _model->Product(_idxInteract)->P4();
+        return _interactIdx == -1 ? GetNominal4Vector() : Model()->Product(_interactIdx)->P4();
     }
-    //if decays return nominal decay p4, if not return my P4
-    const LorentzVector& GetNominal4Vector() const {return _nominal;}
-    
-    Int_t GetInteractingPdg()const {return _interactingPdg;}
-    
-    /*void SetVertexXYZT(double x,double y,double z,double t){
-      _productionVertex.SetXYZT(x,y,z,t);
-      }*/
 
- 
-    void SetAngleThetaPhi(Double_t th,Double_t phi);
-    void SetHorSize(Double_t s){_sizeHor=s;}
-    void SetVerSize(Double_t s){_sizeVer=s;}
-    void SetHorDivergence(Double_t s){_divHor=s;}
-    void SetVerDivergence(Double_t s){_divVer=s;}
+  /**
+   * @brief Add additional decay channel
+   */
+   void AddDecay(double bratio,decaymodel_ptr  mod,decayer_ptr  dec){
+     _decayChannel.AddDecay(nullptr,bratio,std::move(mod),std::move(dec));
+    }
 
+  /**
+   * @brief Define Decay type as production
+   */
+  DecayType IsDecay() const noexcept final {return DecayType::Production;}
+
+  /**
+   * @brief Choose a decay if required
+   */
+    void ChooseDecay() const {
+      if(_decayChannel.N() == 0) return;
+      
+      //Randomly select a decay channel based on branching ratio
+      bool good = false;
+      while(good==false){
+	auto idec = _decayChannel.ChooseDecay(Mass());
+	//recurse daughter particles
+	good = Model()->SampleMasses(Mass());
+	Model()->ChooseDecay();
+      }
+    }
   
-  private:
+private:
+    DecayChannel _decayChannel; ///< The single decay channel owned by this particle.
 
-    decaymodel_ptr _model={nullptr}; //not owner
-    
-    decayer_ptr   _decayer={nullptr}; //owner
+    // Beam and target properties
+    LorentzVector _nominalP4;      ///< Nominal beam four-momentum.
+    Int_t _interactingPdg = 0;      ///< PDG code of the particle this one interacts with.
+    Int_t _interactIdx = -1;        ///< Index of the interacting product particle.
+    BeamProperties _beamProps;      ///< Struct containing all beam-specific properties.
 
-    //  LorentzVector *_interactingParticle={nullptr}; //not owner;
-    
-    LorentzVector _nominal; //nominal beam 4-momentum
-    // LorentzVector _productionVertex;
-    //int _prodVertexID={0};
-    int _interactingPdg={0};
-    Double_t _dirTheta={0};
-    Double_t _dirPhi={0};
-    Double_t _sizeHor={0};
-    Double_t _sizeVer={0};
-    Double_t _divHor={0};
-    Double_t _divVer={0};
+    ClassDef(CollidingParticle, 1);
+};
+} // namespace elSpectro
 
-    Int_t _idxInteract={-1}; //index of interacting particle in model, -1 if no model
-    
-    ClassDefOverride(elSpectro::CollidingParticle,1); //class CollidingParticle
-    
-  };//class CollidingParticle
+#endif
 
-}//namespace elSpectro
